fa40d30c9a3fd8cf5a8b7bbf155a3cdf17e59194
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-20 19:04:48 +0800

    lineage-20: 2026-02-01

---
+++ lineageos-20-ul/libra_20.sh
@@ -0,0 +1,11 @@
+git clone --depth=1 https://github.com/ximi-libra-test/android_device_xiaomi_libra device/xiaomi/libra -b lineage-20-rev
+
+git clone --depth=1 https://github.com/ximi-libra-test/android_vendor_xiaomi_libra vendor/xiaomi/libra -b lineage-20
+
+git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom-caf/msm8994/audio -b lineage-18.1-caf-msm8994
+
+git clone --depth=1 https://github.com/LineageOS-UL/android_hardware_qcom_display hardware/qcom-caf/msm8994/display -b lineage-20.0-caf-msm8994
+
+git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media hardware/qcom-caf/msm8994/media -b lineage-18.1-caf-msm8994
+
+
